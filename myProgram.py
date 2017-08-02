0209397bb96b0f5d2704d02930a18dc39a22e53a
AngelCampos <angel.campos@weizmann.ac.il>
2017-08-02 13:48:16 +0300

    Adding functionality

--- myProgram.py
+++ myProgram.py
@@ -7,7 +7,7 @@ import argparse
 # Arguments parser
 parser = argparse.ArgumentParser(description=
 "****************************************************************************\n"
-"This code will take 2 numbers and add them \n"
+"This code will take 2 numbers and add them or multiply them (Default=ADD)\n"
 "****************************************************************************")
 parser.add_argument("x", type=int, help="First number")
 parser.add_argument("y", type=int, help="Second number")
@@ -15,10 +15,18 @@ parser.add_argument("-v", "--verbose", action="store_true",
     help = "Richer output")
 parser.add_argument("-s", "--sum", action="store_true", default=True, 
     help = "Select sum operation")
-args = parser.parse_args()
+parser.add_argument("-m", "--multiply", action="store_true", 
+    help = "Select multiply operation")
+args = qarser.parse_args()
 
 # Main Program
-if args.sum:
+if args.multiply:
+    answer = args.x * args.y
+    if args.verbose:
+        print("{} multiplied by {} equals {}".format(args.x, args.y, answer))
+    else:
+        print(answer)
+elif args.sum:
     answer = args.x + args.y
     if args.verbose:
         print("{} summed with {} equals {}".format(args.x, args.y, answer))
